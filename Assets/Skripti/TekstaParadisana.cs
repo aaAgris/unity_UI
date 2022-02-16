ef0d1893d404f173a444df3ea650b8d1d64f1ca5
Antans <antansagris@gmail.com>
2022-02-16 14:14:51 +0200

    Darbibas ar tekstu un pogu

---
+++ Assets/Skripti/TekstaParadisana.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TekstaParadisana : MonoBehaviour {
+
+		//zugalba tekstu kurs panemts no input lauka
+	public string teksts;
+	//lauks no kurua panemt tekstu
+	public GameObject ievadesLauks;
+	//tekstsa objekts kur atttelot mainiga teksts saturu
+	public GameObject tekstalauks;
+
+	public void darbibasArTekstu(){
+		teksts = ievadesLauks.GetComponent<Text> ().text;
+		tekstalauks.GetComponent<Text> ().text = "Sveiks " + teksts.ToUpper () + " !";
+
+
+	}
+		
+
+}
