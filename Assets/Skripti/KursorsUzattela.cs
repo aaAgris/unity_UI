c5859aaea5211d3a289999522ccbf3846c81f764
Antans <antansagris@gmail.com>
2022-02-22 15:27:35 +0200

    gatavs beans

---
+++ Assets/Skripti/KursorsUzattela.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KursorsUzattela : MonoBehaviour {
+	public AudioSource skanasAvots;
+	public AudioClip skanaKoAtskanot;
+
+	public void uzbiditsUzAttela(){
+		skanasAvots.PlayOneShot(skanaKoAtskanot);
+	}
+
+	public void nobiditsNoAttela(){
+		skanasAvots.Stop ();
+	}
+
+}
---
+++ Assets/Skripti/ObjektuParvietosana.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//interfeiss lai nodrosinatu peles funkcijas
+using UnityEngine.EventSystems;
+
+
+public class ObjektuParvietosana : MonoBehaviour, IPointerDownHandler, IBeginDragHandler,IDragHandler,IEndDragHandler {
+
+	private RectTransform transformacijasLogs;
+	public Canvas kanva;
+
+	void Start(){
+		transformacijasLogs = GetComponent<RectTransform> ();
+	}
+	public void OnPointerDown(PointerEventData notikums){
+		Debug.Log ("Kreisais Klikskis uz objekta!");
+
+	}
+
+	public void OnBeginDrag(PointerEventData notikums){
+		Debug.Log ("Uzsakta objekta vilksana!");
+
+	}
+
+	public void OnDrag(PointerEventData notikums){
+		transformacijasLogs.anchoredPosition += notikums.delta / kanva.scaleFactor;
+
+	}
+
+	public void OnEndDrag(PointerEventData notikums){
+		Debug.Log ("Objekta vilksana partraukta !");
+
+	}
+
+
+
+
+}
