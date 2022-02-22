bad8ea3e5be08ea21fe9bfc3aaec643d3528af8c
Antans <antansagris@gmail.com>
2022-02-22 10:43:36 +0200

    skana

--- Assets/Skripti/AtteluParadisana.cs
+++ Assets/Skripti/AtteluParadisana.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AtteluParadisana : MonoBehaviour {
 
@@ -8,6 +9,9 @@ public class AtteluParadisana : MonoBehaviour {
 	public GameObject lacitits;
 	public GameObject tante;
 	public GameObject masina;
+	public GameObject attelaTuretajs;
+	public Sprite[] attelumasivs;
+	public GameObject slaideris;
 
 	public void binaAttelosana(bool vertiba){
 		bins.SetActive (vertiba);
@@ -33,7 +37,22 @@ public class AtteluParadisana : MonoBehaviour {
 		bins.transform.localScale = new Vector2 (-1, 1);
 	}
 
+	public void izkritosais(int skaitlis ){
+		if (skaitlis == 0)
+			attelaTuretajs.GetComponent<Image> ().sprite = attelumasivs [0];
+		else if (skaitlis == 1)
+			attelaTuretajs.GetComponent<Image> ().sprite = attelumasivs [1];
+		else if (skaitlis == 2)
+			attelaTuretajs.GetComponent<Image> ().sprite = attelumasivs [2];
+	}
 
+
+	public void mainitLielumu(){
+		float pasreizejaVertiba = slaideris.GetComponent<Slider> ().value;
+		attelaTuretajs.transform.localScale = new Vector2 (1F*pasreizejaVertiba,1F*pasreizejaVertiba);
+
+
+	}
 
 
 
